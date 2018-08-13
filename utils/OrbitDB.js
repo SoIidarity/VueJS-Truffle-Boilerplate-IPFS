3057cb85a04282ebac087d49d896647420340ea8
SoIidarity <christopher.maree@gmail.com>
2018-08-13 17:25:47 +0200

    Added basic logic to load existing DB

--- utils/OrbitDB.js
+++ utils/OrbitDB.js
@@ -17,19 +17,26 @@ const ipfs = new IPFS({
 //This implementation uses IPFS log. this can be easily changed to other forms, such as document store
 ipfs.on('ready', async () => {
     orbitdb = new OrbitDB(ipfs)
-    db = await orbitdb.eventlog('SampleLogDB')
+})
 
+const createDB = async (name) => {
+    db = await orbitdb.eventlog(name)
     console.log("DB address")
     console.log(db.address.toString())
+}
 
-})
+const loadDB = async (address) => {
+    db = await orbitdb.open(address, {sync: true})
+    console.log("DB address")
+    console.log(db.address.toString())
+}
 
 const addValueToLog = async (c) => {
     db.add(c)
 }
 
 const getValuesFromLog = async (c) => {
-    return db.iterator({ limit: 5 }).collect()
+    return db.iterator({limit: 5}).collect()
 }
 
-export {addValueToLog, getValuesFromLog}
+export {createDB, loadDB, addValueToLog, getValuesFromLog}
